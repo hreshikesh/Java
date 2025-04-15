587188fc1d4eb5c5e96550ba0a1ea9003a2486d6
Hreshikesha K <ailhreshikesh@gmail.com>
2025-04-16 00:14:03 +0530

    Updated File

--- Practice/src/com/xworkz/practice/Internal/ArmyVehicle.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.practice.Internal;
-
-public class ArmyVehicle {
-    void wheel(Rubber rubber) {
-        rubber.expand();
-    }
-}
--- Practice/src/com/xworkz/practice/Internal/Rubber.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.practice.Internal;
-
-public class Rubber {
-    void expand() {
-        System.out.println("Running Expand in Rubber");
-    }
-}
