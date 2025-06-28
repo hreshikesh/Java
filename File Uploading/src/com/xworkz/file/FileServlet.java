867ba7bf22c67cfeca0b1f798d22474a149f54a7
Hreshikesh <ailhreshikesh@gmail.com>
2025-06-28 19:00:27 +0530

    File Uplaoding and Saving into directory

--- File Uploading/src/com/xworkz/file/FileServlet.java
+++ File Uploading/src/com/xworkz/file/FileServlet.java
@@ -1,15 +1,32 @@
 package com.xworkz.file;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.File;
 import java.io.IOException;
+@MultipartConfig(fileSizeThreshold = 1024*1024,maxFileSize = 1024*1024*10,maxRequestSize = 1024*1024*50)
 @WebServlet(urlPatterns = "/upload",loadOnStartup = 1)
 public class FileServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Part part=req.getPart("file");
+        System.out.println(part);
+        //getting the file name
+        String fileName=part.getSubmittedFileName();
+        System.out.println(fileName);
+        //loading upload path
+        String uploadPath="C:\\Users\\ailhr\\Downloads";
+        File file=new File(uploadPath);
+        System.out.println(file);
+
+
+
 
     }
 }
--- File Uploading/static/index.jsp
+++ File Uploading/static/index.jsp
@@ -6,9 +6,10 @@
 <h1>
     Upload File
 </h1>
-<form action="upload" method="post">
+<form action="upload" method="post" enctype="multipart/form-data">
 <label for="fileId">File</label>
 <input type="file" name="file" id="fileId" required>
+    <button type="submit">submit</button>
 </form>
 </body>
 </html>
