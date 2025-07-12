a029ded824b74f928def8b07600a8a912608ae80
Hreshikesh <ailhreshikesh@gmail.com>
2025-07-12 18:54:38 +0530

    Optional Class

--- Optional/src/com/xworkz/optional/Runner/RainRunner.java
+++ Optional/src/com/xworkz/optional/Runner/RainRunner.java
@@ -8,8 +8,13 @@ public class RainRunner {
     public static void main(String[] args) {
         Rain rain=new Rain();
         Optional<Integer> amount =rain.rainAmount("bangalore");
+        Optional<Integer> amount1 =rain.rainAmount("mysore");
         if(amount.isPresent()){
-            int rainAmount =amount.get();
+            int rainAmount = amount.get();
+            System.out.println("Rain amount "+ rainAmount);
+        }
+        if(amount1.isPresent()){
+            int rainAmount = amount1.get();
             System.out.println("Rain amount "+ rainAmount);
         }
 
