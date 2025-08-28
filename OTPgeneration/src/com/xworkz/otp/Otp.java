24c0fa4ea2a7c7825c801533576beb7e14031a70
Hreshikesh <ailhreshikesh@gmail.com>
2025-08-28 16:32:40 +0530

    OTP Generation

--- OTPgeneration/src/com/xworkz/otp/Otp.java
+++ OTPgeneration/src/com/xworkz/otp/Otp.java
@@ -4,27 +4,26 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Otp {
+   private static StringBuilder a=new StringBuilder();
+   private static Random random=new Random();
+    public static StringBuilder generateOtp(){
 
-    public static int generateOtp(){
-        int a = 0;
-        Random random=new Random();
         for (int i = 0; i <6 ; i++) {
-            a=random.nextInt(999999);
+            a.append(random.nextInt(10));
         }
-
         return a;
     }
 
 
 
     public static void main(String[] args) {
-        int otp=generateOtp();
+        StringBuilder otp=generateOtp();
         System.out.println(otp);
 
         System.out.println("Enter your otp");
         Scanner scanner=new Scanner(System.in);
-        int enteredOtp=scanner.nextInt();
-        if(enteredOtp==otp){
+        StringBuilder enteredOtp= new StringBuilder(scanner.next());
+        if(enteredOtp.toString().equals(otp.toString())){
             System.out.println("OTP Successfully verified");
         }else System.err.println("OTP not equal");
     }
