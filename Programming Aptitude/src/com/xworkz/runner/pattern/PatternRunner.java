d805e55b58309f1e869d354b4f501c16d9268579
Hreshikesh <ailhreshikesh@gmail.com>
2025-08-29 11:29:07 +0530

    Programming

--- Programming Aptitude/src/com/xworkz/runner/pattern/PatternRunner.java
+++ Programming Aptitude/src/com/xworkz/runner/pattern/PatternRunner.java
@@ -10,23 +10,23 @@ public class PatternRunner {
             }
             System.out.println("\n");
 
-            
+
         }
 
 
         System.out.println("2.");
-        for (int i = 0; i <=5; i++) {
-            for (int j = 0; j <i; j++) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println("\n");
-            
+
         }
 
 
         System.out.println("3.");
-        for (int i = 5; i >=0 ; i--) {
-            for (int j = 0; j <i ; j++) {
+        for (int i = 5; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println("\n");
@@ -34,8 +34,8 @@ public class PatternRunner {
         }
 
         System.out.println("4.");
-        for (int i = 1; i <=5 ; i++) {
-            for (int j = 1; j <=i ; j++) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
             System.out.println("\n");
@@ -43,18 +43,33 @@ public class PatternRunner {
 
 
         System.out.println("5.");
-        for (int i = 1; i <=5; i++) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+
+
+        System.out.println("6.");
+
+        for (int i = 1; i <=5 ; i++) {
+            for (int j = i; j <5 ; j++) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <=i ; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("\n");
+
         }
-        for (int i = 5; i >=1 ; i--) {
-            for (int j = 1; j <=i; j++) {
-                System.out.print("*");
-            }
-            System.out.println("\n");
-        }
+
 
     }
 }
