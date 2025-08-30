095cf092148c79deecb41a826e381035a46c0d20
Hreshikesh <ailhreshikesh@gmail.com>
2025-08-30 15:51:02 +0530

    Programming

--- Programming Aptitude/src/com/xworkz/runner/pattern/PatternRunner.java
+++ Programming Aptitude/src/com/xworkz/runner/pattern/PatternRunner.java
@@ -155,6 +155,28 @@ public class PatternRunner {
         }
 
 
+        System.out.println("12.");
+        for (int i = 1; i <=num ; i++) {
+            for (int j = 1; j <i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <=num ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <=num ; i++) {
+            for (int j = i; j <num ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
     }
 
 
