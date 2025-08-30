dd8f3628273e07cdd6552b5c0ea2b34c189fb802
Hreshikesh <ailhreshikesh@gmail.com>
2025-08-30 16:21:02 +0530

    Programming

--- Programming Aptitude/src/com/xworkz/runner/pattern/PatternRunner.java
+++ Programming Aptitude/src/com/xworkz/runner/pattern/PatternRunner.java
@@ -177,7 +177,57 @@ public class PatternRunner {
         }
 
 
+        System.out.println("13.Hollow traingle");
+        for (int i = 1; i <=num ; i++) {
+            for (int j = i; j <num ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=(2*i)-1; j++) {
+                if (j == 1||j==(2*i)-1||i==num) {
+                    System.out.print("*");
+                }else System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("14.");
+        for (int i = num; i >=1 ; i--) {
+            for (int j = 1; j <=num-i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j =1; j <= (2*i)-1 ; j++) {
+                if (j==1||j==(2*i)-1||i==num)
+                System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println("15.Hollow diamond");
+        for (int i = 1; i <=num ; i++) {
+            for (int j = i; j <num ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=(2*i)-1 ; j++) {
+                if (j==1||j==(2*i)-1)
+                System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        for (int i = num; i >=1 ; i--) {
+            for (int j = 1; j <=num-i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=(2*i)-1 ; j++) {
+                if(j==1||j==(2*i)-1)
+                    System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+
+        }
     }
-
-
 }
